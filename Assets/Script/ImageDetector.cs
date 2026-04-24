6aedeb1ed67a99f158d43459408d2bc1a77deeb8
Hakeron <richardyerna@gmail.com>
2026-04-24 14:29:47 +0200

    Canvas

--- Assets/Script/ImageDetector.cs
+++ Assets/Script/ImageDetector.cs
@@ -33,32 +33,39 @@ public class ImageDetector : MonoBehaviour
 
     private void OnChanged(ARTrackablesChangedEventArgs<ARTrackedImage> eventArgs)
     {
-        var text = textComponent.GetComponent<TextMesh>();
+        var text = textComponent.GetComponentInChildren<TMP_Text>();
         foreach (ARTrackedImage image in eventArgs.added)
         {
+            textComponent.SetActive(true);
             string imageName = image.referenceImage.name;
             Vector3 spawnPosition = image.transform.position;
+            textComponent.transform.SetParent(image.transform);
+            textComponent.transform.localPosition = Vector3.zero+new Vector3(1,0,0);
             Debug.Log("Image Added: "+imageName);
             switch (imageName)
             {
                 case "one":
-                    text.text = "Test one ";
+                    text.text = "Ceci est un 1 ";
                     
                     break;
                 case "two":
-                    text.text = "Test two ";
+                    text.text = "Ceci est un 2 ";
                     
                     break;
                 case "Rafflesia":
-                    text.text = "Test rafflesia ";
+                    text.text = "Magnifique fleur ";
                     
                     break;
                 case "QRCode":
-                    text.text = "Test QRCode ";
+                    text.text = "Ce QR Code ne sert à rien ";
                     
                     break;
                 case "unitylogowhiteonblack":
-                    text.text = "Test unitylogowhiteonblack ";
+                    text.text = "Beau moteur ! ";
+                    
+                    break;
+                case "GoldenTrumpet":
+                    text.text = "J'ai pas compris ce que c'était ";
                     
                     break;
 
@@ -92,7 +99,7 @@ public class ImageDetector : MonoBehaviour
     {
         Vector3 direction = textComponent.transform.position - _camera.transform.position;
         direction = Vector3.ProjectOnPlane(direction, Vector3.up);
-        Quaternion targetRotation = Quaternion.LookRotation(direction);
+        textComponent.transform.rotation = Quaternion.LookRotation(direction);
     }
     
 
